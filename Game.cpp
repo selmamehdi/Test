bb9357b5b94e3a0cd0bac5a10111596436d9111d
selmamehdi <selmamehdi@18ed7596-6daf-47d9-b88f-4ddf7113fed2>
2017-11-22 08:01:47 +0000

    iput the initial code source of the game, every one will take his part and start coding as we agreed

---
+++ Game.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include "personnage.h"
+#include "Magicien.h"
+#include "Guerrier.h"
+#include "faire_rencontrer.cpp"
+
+int main(){
+
+}
+
+/*
+int main(){
+    // Creation de l'objet MAGICIEN & GUERIER
+    Magicien M(100,100,"Wizard","Magic Stick");
+    Guerrier G(100,100,"Knight","Sword of Justice");
+
+    // Affichage des objets
+    M.affiche();
+    G.affiche();
+    //M.rencontrer(G);
+    faire_rencontrer(M,G);
+    G.set_vie(30);
+    cout<<endl;
+    cout<<"la vie de Guerrier "<<G.get_name()<<" devient "<<G.get_vie()<<" danc il decide d'utiliser un autre arme"<<endl;
+    G.set_arme("The Ultimate Sword");
+    G.affiche();
+    //G.rencontrer(M);
+    faire_rencontrer(G,M);
+    M.set_vie(0);
+    M.affiche();
+
+    return 0;
+}
+*/
