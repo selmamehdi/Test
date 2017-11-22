064c6645ef85d9fab5f4315051be3eaa412cdd84
selmamehdi <selmamehdi@18ed7596-6daf-47d9-b88f-4ddf7113fed2>
2017-11-22 08:01:37 +0000

    iput the initial code source of the game, every one will take his part and start coding as we agreed

---
+++ faire_rencontrer.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include "personnage.h"
+
+void faire_rencontrer(Personnage& P1, Personnage& P2){
+
+
+}
+/*
+void faire_rencontrer(Personnage& P1, Personnage& P2){
+
+    cout<<P1.get_name()<<" rencontre "<<P2.get_name()<<endl;
+    P1.rencontrer(P2);
+}
+*/
